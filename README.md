91465c993103a10095791d7780e4946f13a45f2c
Abdullah DURSUN <adursun@outlook.com.tr>
2019-09-21 15:33:45 +0300

    Add README

---
+++ README.md
@@ -0,0 +1,18 @@
+# WSDDN PyTorch
+
+## Requirements
+
+- Docker (19.03.2)
+- nvidia-container-toolkit (https://github.com/NVIDIA/nvidia-docker)
+
+## Build Steps
+
+```
+./prepare.sh
+docker run --gpus all -v `pwd`:/ws -it wsddn.pytorch /bin/bash
+```
+
+## Training Steps
+```
+python3 wsddn-pytorch.py
+```
---
+++ prepare.sh
@@ -0,0 +1,19 @@
+# download and extract VOC 2007 trainval split
+wget http://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar -P data/
+mkdir data/VOCtrainval_06-Nov-2007
+tar xf data/VOCtrainval_06-Nov-2007.tar --directory data/VOCtrainval_06-Nov-2007/
+
+# download and extract VOC 2007 test split
+wget http://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar -P data/
+mkdir data/VOCtest_06-Nov-2007
+tar xf data/VOCtest_06-Nov-2007.tar --directory data/VOCtest_06-Nov-2007/
+
+# download and extract selective search windows boxes
+wget http://www.cs.cmu.edu/~spurushw/hw2_files/selective_search_data.tar -P data/
+tar xf data/selective_search_data.tar --directory data/
+
+# download pretrained alexnet weights
+wget https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth -P states/
+
+# build the docker image
+docker build . -t wsddn.pytorch
