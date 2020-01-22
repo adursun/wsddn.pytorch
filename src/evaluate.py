237b93335c02de4c819104648a42c58f6ffbe709
Abdullah Dursun <adursun@outlook.com.tr>
2020-01-22 18:39:07 +0300

    Add vgg support to evaluation

--- src/evaluate.py
+++ src/evaluate.py
@@ -13,10 +13,13 @@ DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Evaluate trained model")
+    parser.add_argument(
+        "--base_net", type=str, default="alexnet", help="Base network to use"
+    )
     parser.add_argument("--state_path", help="Path of trained model's state")
     args = parser.parse_args()
 
-    net = WSDDN()
+    net = WSDDN(base_net=args.base_net)
     net.load_state_dict(torch.load(args.state_path))
     net.to(DEVICE)
 
